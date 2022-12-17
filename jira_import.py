a918e678f9a2d762f67f74f520363a85709b0da9
jean <jean.gd@outlook.fr>
2022-12-17 20:47:49 +0100

    New version

--- jira_import.py
+++ jira_import.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 ---------------------------------------------------------------------------------------------------------------
+Title: Transform an XML document to a pandas dataframe to generate Jira reports more quickly
 
-Remark : This code allows to import jira data which are in JSON format to a pandas
-dataframe. This part of program has been developed for the Moko application.
+Remark : The main objective is to generate reports containing tables that contain jira data in a Word or Excel
+format. These tables are easily defined in XML documents (for more information : Readme.md).
 
-Written by : Jean Guiraud
-
+Written by: Jean Guiraud
 ---------------------------------------------------------------------------------------------------------------
 """
 
@@ -16,7 +16,6 @@ Written by : Jean Guiraud
 import os
 import sys
 import xml.etree.ElementTree as ET
-
 import docx
 import pandas as pd
 from jira.client import JIRA
@@ -29,9 +28,8 @@ class Jira_XMLDocument:
     """
     Transform XML Document from Jira server to a pandas dataframe
 
-    **params**
-        - **jira** = jira instance
-        - **xml** = link to the XML document
+    :param jira: jira instance
+    :param xml: link to the XML document
     """
 
     def __init__(self, jira: JIRA, xml: str):
@@ -54,7 +52,7 @@ class Jira_XMLDocument:
 
             if table.get("style") == "Classic":
                 jira_issues = jira.search_issues(table.find("JQL").text, maxResults=False)
-                self.file.append(pd.DataFrame(self.__jira_import__(jira_issues, tab)))
+                self.file.append(pd.DataFrame(jira_import(jira_issues, tab)))
 
             elif table.get("style") == "MultipleFilters":
 
@@ -63,7 +61,7 @@ class Jira_XMLDocument:
                 for filters in table.findall("Filters"):
                     for nbJQL in filters.findall("JQL"):
                         jira_issues = jira.search_issues(nbJQL.text, maxResults=False)
-                        pandas_tables.append(pd.DataFrame(self.__jira_import__(jira_issues, tab)))
+                        pandas_tables.append(pd.DataFrame(jira_import(jira_issues, tab)))
 
                 self.file.append(pandas_tables)
 
@@ -74,14 +72,13 @@ class Jira_XMLDocument:
                     'issue in linkedIssues(' + jira_issues[0].key + ', ' +
                     table.find("JQL").get("link") + ')', maxResults=False)
 
-                self.file.append(pd.DataFrame(self.__jira_import__(jira_issues, tab)))
+                self.file.append(pd.DataFrame(jira_import(jira_issues, tab)))
 
     def to_excel(self, document_name: str = "jira_excel"):
         """
         Generate Excel document
 
-        **params**
-            - **docname** = the document name *(default : jira_excel)*
+        :param document_name: the document name *(default : jira_excel)*
                 / you can add the path, like : users/john/test
         """
 
@@ -149,23 +146,21 @@ class Jira_XMLDocument:
         wb.Close(True)
         """
 
-    def to_word(self):
+    def to_word(self, document_name: str = "jira_word"):
         """
-        Generate Word document
+        Generate Word document from Jira_XMLDocument instance
 
-        **params**
-            - **docname** = the document name *(default : jira_word)*
+        :param document_name: the document name *(default : jira_word)*
         """
 
         pass  # TODO
 
-    def to_word_template(self, path_template_word: str, document_name: str = "jira_word_template"):
+    def to_word(self, path_template_word: str, document_name: str = "jira_word_template"):
         """
-        Generate Word template document
+        Generate Word template document from Jira_XMLDocument instance
 
-        **params**
-            - **path_template_word = path of the word template *(see Readme.md)*
-            - **docname** = the document name *(default : jira_word_template)*
+        :param path_template_word: path of the word template *(see Readme.md)*
+        :param document_name: the document name *(default : jira_word_template)*
                 / you can add the path, like : users/john/test
         """
 
@@ -175,23 +170,23 @@ class Jira_XMLDocument:
 
         for incremental, all_tables in enumerate(self.root.findall('Table')):
 
-            for searchtables in range(len(tables)):
+            for search in range(len(tables)):
                 try:
-                    if all_tables.get("keyword") == tables[searchtables].cell(1, 0).text:
-                        tableslenght = searchtables
+                    if all_tables.get("keyword") == tables[search].cell(1, 0).text:
+                        tables_length = search
                 except:
                     continue
 
-                searchtables += 1
+                search += 1
 
             if all_tables.get("style") == "Classic" or all_tables.get('style') == "LinkOneTicket":
 
                 # add the rest of the data frame
                 for i in range(self.file[incremental].shape[0]):
                     if i != 0:
-                        tables[tableslenght].add_row()
+                        tables[tables_length].add_row()
                     for j in range(self.file[incremental].shape[-1]):
-                        tables[tableslenght].cell(i + 1, j).text = str(self.file[incremental].values[i, j])
+                        tables[tables_length].cell(i + 1, j).text = str(self.file[incremental].values[i, j])
 
             elif all_tables.get("style") == "MultipleFilters":
 
@@ -201,165 +196,165 @@ class Jira_XMLDocument:
                     for inc_JQL, nbJQL in enumerate(filters.findall("JQL")):
 
                         if row != 1:
-                            tables[tableslenght].add_row()
+                            tables[tables_length].add_row()
 
-                        tables[tableslenght].cell(row, 0).text = nbJQL.get("name")
-                        self.__make_rows_bold__(tables[tableslenght].rows[row])
+                        tables[tables_length].cell(row, 0).text = nbJQL.get("name")
+                        __make_rows_bold__(tables[tables_length].rows[row])
 
                         for column in range(self.file[incremental][inc_JQL].shape[-1]):
-                            tables[tableslenght].cell(row, 0).merge(tables[tableslenght].cell(row, column))
+                            tables[tables_length].cell(row, 0).merge(tables[tables_length].cell(row, column))
 
-                        tables[tableslenght].cell(row, 0).paragraphs[
+                        tables[tables_length].cell(row, 0).paragraphs[
                             0].alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
 
                         row += 1
 
                         # add the rest of the data frame
                         for i in range(self.file[incremental][inc_JQL].shape[0]):
-                            tables[tableslenght].add_row()
+                            tables[tables_length].add_row()
                             for j in range(self.file[incremental][inc_JQL].shape[-1]):
-                                tables[tableslenght].cell(row, j).text = str(
+                                tables[tables_length].cell(row, j).text = str(
                                     self.file[incremental][inc_JQL].values[i, j])
                             row += 1
         # save the doc
-        document.save(path + document_name + '.docx')
-
-    def __make_rows_bold__(self, *rows):
-        """
-        Set a row in bold
-
-        **param**
-            - **row** = python-docx row
-        """
-
-        for row in rows:  # Select all rows
-            for cell in row.cells:  # Select all cells
-                for paragraph in cell.paragraphs:  # Select all cell's paragraph
-                    for run in paragraph.runs:  # Select all paragraph's run
-                        run.font.bold = True  # Set in bold the run
-
-    def __just_highest_issues__(self, splitter: str, n_splitter: str, version: int, list_to_split: list) -> list:
-        """
-        Keep the highest issue from a list where issue is located in a text box with separators
-
-        **params**
-            - **splitter** = the separator
-            - **n_splitter** = the position of the issue name *(example : issue_2 the position is 0 : issue)*
-            - **version** = the position of the version that you want to compare *(example issue_2 is the position 1 : 2)*
-            - **list_to_split** = your list where you want to keep the highest issue
-        """
-
-        list_to_split.sort()
-        list_highest_issue = []
-
-        val = 0
-        while val < len(list_to_split) - 1:
-            max = 0
-            groupby = element = list_to_split[val].split(splitter)
-
-            while element[n_splitter] == groupby[n_splitter]:
-
-                if max < int(element[version]):
-                    max = int(element[version])
-                    maxInd = val
-
-                val += 1
-
-                if val > len(list_to_split) - 1:
-                    break
-
-                element = list_to_split[val].split(splitter)
-
-            list_highest_issue.append(list_to_split[maxInd])
-
-        return list_highest_issue
-
-    def __jira_import__(self, jira_issues: dict, information: dict) -> list:
-        """
-        Extract data from jira to a classic list
-
-        **params**
-            1. **jira_issues** = jira ResultList[] *(dictionary)*
-            2. **information** = dictionary that contains 1 key and 2 values:
-                - *Key*, index of the column (1, 2, 3, etc...)
-                - *Value 1*, data to extract (example : summary, customfield, description, etc...)
-                - *Value 2*, type of data (example : multiplevalue, link, etc...)
-
-                    *example : {0: ['', 'summary'], 1: ['', 'summary'], 2: ['', 'description']}*
-        """
-
-        jira_import = []
-
-        if len(jira_issues) != 0:
-
-            for incremental, all_issues in enumerate(jira_issues):  # Browse all issues in the JSON file
-
-                jira_import.append({})
-
-                for inc, table in information.items():
-
-                    # Condition for the multiple values in the JSON variable
-                    if table[0] == "multiplevalues":  # To check that it is called in the XML file
-
-                        Fulltext = ""  # Variable that adds the names of the multiple values
-                        for multiplevalues in eval("all_issues.fields." + table[1]):
-                            if Fulltext == "":
-                                Fulltext = str(multiplevalues.name)
-                            else:
-                                Fulltext = Fulltext + "\n" + str(multiplevalues.name)
-
-                        jira_import[incremental][table[0]] = Fulltext
-
-                    elif table[0] == "link":
-
-                        Fulltext = []
-
-                        for link in all_issues.fields.issuelinks:
-                            if link.type.inward == table[1]:
-                                if hasattr(link, "inwardIssue"):
-                                    cut_string = str(link.inwardIssue.fields.summary).split(
-                                        "_")  # To get the name of the issue
-
-                                    Fulltext.append(str(cut_string[0]) + " Iss. " + str(cut_string[1]))
-
-                            elif link.type.outward == table[1]:
-                                if hasattr(link, "outwardIssue"):
-                                    cut_string = str(link.outwardIssue.fields.summary).split(
-                                        "_")  # To get the name of the issue
-
-                                    Fulltext.append(str(cut_string[0]) + " Iss. " + str(cut_string[1]))
-
-                        Fulltext = self.__just_highest_issues__(" Iss. ", 0, Fulltext)
-                        Text = ""
-
-                        for Fulltext in Fulltext:
-
-                            if Text == "":
-                                Text += Fulltext
-                            else:
-                                Text += "\n" + Fulltext
-
-                        jira_import[incremental][table[1]] = Text
-
-                    elif table[0] == "summary":
-                        cut_string = str(all_issues.fields.summary).split("_")  # Split variable
-                        jira_import[incremental][table[1]] = cut_string[2]
-
-                    # For all other fields
-                    else:
-                        value = table[1]
-                        if value in jira_import[incremental]:
-                            value += str(inc)
-                        jira_import[incremental][value] = str(eval("all_issues.fields." + table[1]))
-
-        return jira_import
+        document.save(document_name + '.docx')
 
     def __str__(self) -> str:
         return "Jira_XMLDocument : " + self.root.attrib["name"]
 
 
+def jira_import(jira_issues: dict, information: dict) -> list:
+    """
+    Extract data from jira to a classic list
+
+    :param jira_issues; jira ResultList[] *(dictionary)*
+    :param information: dictionary that contains 1 key and 2 values:
+            - Key = index of the column (1, 2, 3, etc...)
+            - Value 1 = data to extract (example : summary, customfield, description, etc...)
+            - Value 2 = type of data (example : multiplevalue, link, etc...)
+            example : {0: ['', 'summary'], 1: ['', 'summary'], 2: ['', 'description']}
+    """
+
+    import_array = []
+
+    if len(jira_issues) != 0:
+
+        for incremental, all_issues in enumerate(jira_issues):  # Browse all issues in the JSON file
+
+            import_array.append({})
+
+            for inc, table in information.items():
+
+                # Condition for the multiple values in the JSON variable
+                if table[0] == "multiple_values":  # To check that it is called in the XML file
+
+                    Fulltext = ""  # Variable that adds the names of the multiple values
+                    for multiple_values in eval("all_issues.fields." + table[1]):
+                        if Fulltext is None:
+                            Fulltext = str(multiple_values.name)
+                            continue
+                        Fulltext += "\n" + str(multiple_values.name)
+
+                    import_array[incremental][table[0]] = Fulltext
+
+                elif table[0] == "link":
+
+                    Fulltext = []
+
+                    for link in all_issues.fields.issuelinks:
+                        if link.type.inward == table[1]:
+                            if hasattr(link, "inwardIssue"):
+                                cut_string = str(link.inwardIssue.fields.summary).split(
+                                    "_")  # To get the name of the issue
+
+                                Fulltext.append(str(cut_string[0]) + " Iss. " + str(cut_string[1]))
+
+                        elif link.type.outward == table[1]:
+                            if hasattr(link, "outwardIssue"):
+                                cut_string = str(link.outwardIssue.fields.summary).split(
+                                    "_")  # To get the name of the issue
+
+                                Fulltext.append(str(cut_string[0]) + " Iss. " + str(cut_string[1]))
+
+                    Fulltext = __just_highest_issues__(splitter=" Iss. ", n_splitter=0, version=1, list_to_split=Fulltext)
+
+                    Text = ""
+                    for Fulltext in Fulltext:
+                        if Text is None:
+                            Text = Fulltext
+                            continue
+                        Text += "\n" + Fulltext
+
+                    import_array[incremental][table[1]] = Text
+
+                elif table[0] == "specific_summary":
+                    cut_string = str(all_issues.fields.summary).split("_")  # Split variable
+                    import_array[incremental][table[1]] = cut_string[2]
+
+                # For all other fields
+                else:
+                    value = table[1]
+                    if value in import_array[incremental]:
+                        value += str(inc)
+                    import_array[incremental][value] = str(eval("all_issues.fields." + table[1]))
+
+    return import_array
+
+
+def __just_highest_issues__(splitter: str, n_splitter: str, version: int, list_to_split: list) -> list:
+    """
+    Keep the highest issue from a list where issue is located in a text box with separators
+
+    :param splitter: the separator
+    :param n_splitter: the position of the issue name *(example : issue_2 the position is 0 : issue)*
+    :param version: the position of the version that you want to compare *(example issue_2 is the position 1 : 2)*
+    :param list_to_split: your list where you want to keep the highest issue
+    """
+
+    list_to_split.sort()
+    list_highest_issue = []
+
+    val = 0
+    while val < len(list_to_split) - 1:
+        max = 0
+        group = element = list_to_split[val].split(splitter)
+
+        while element[n_splitter] == group[n_splitter]:
+
+            if max < int(element[version]):
+                max = int(element[version])
+                maxInd = val
+
+            val += 1
+
+            if val > len(list_to_split) - 1:
+                break
+
+            element = list_to_split[val].split(splitter)
+
+        list_highest_issue.append(list_to_split[maxInd])
+
+    return list_highest_issue
+
+
+def __make_rows_bold__(*rows):
+    """
+    Set a row in bold
+
+    :param *rows: python-docx row
+    """
+
+    for row in rows:  # Select all rows
+        for cell in row.cells:  # Select all cells
+            for paragraph in cell.paragraphs:  # Select all cell's paragraph
+                for run in paragraph.runs:  # Select all paragraph's run
+                    run.font.bold = True  # Set in bold the run
+
+
 if __name__ == "__main__":
+
     jira = JIRA(options={'server': ""}, basic_auth=("", ""))
     jira_XML = Jira_XMLDocument(jira, "test.xml")
-    jira_XML.to_excel(path="/Users/jean/Desktop")
-    print("finished")
+    jira_XML.to_excel(document_name="/Users/jean/Desktop/test")
+    print("finished!")
+
